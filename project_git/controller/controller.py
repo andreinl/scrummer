9930704544f749dc63f5e8d456702ea480c418fa
Idealis Consulting <2088903+idealisconsultingbe@users.noreply.github.com>
2019-03-14 16:46:55 +0100

    Fix commit_author_data, line 346

--- project_git/controller/controller.py
+++ project_git/controller/controller.py
@@ -343,7 +343,7 @@ class GitController(http.Controller):
                 ('email', '=', commit_author_data.get("email", ''))
             ], limit=1)
             if author_user:
-                commit_author["user_id"] = author_user.id
+                commit_author_data["user_id"] = author_user.id
 
             if commit_author:
                 commit_author.write(commit_author_data)
