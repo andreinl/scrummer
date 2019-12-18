8405a3178081ab3f09a7c803b91bc82c1f19e067
Mathieu Benoit <mathieu.benoit@mathben.tech>
2019-12-18 13:39:06 -0500

    [MIG] project_git: Migration to 12.0

--- project_git/__manifest__.py
+++ project_git/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Project Git",
     "summary": "Integrates your projects with git based services",
     "category": "Project",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "author": "Odoo Community Association (OCA), Modoolar",
     "website": "https://www.modoolar.com/",
